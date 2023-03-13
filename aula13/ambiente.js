2741dcee29410181fd3492c49abc12dc9ce2878d
Evandro Santana filho <123319087+EvandroSFilho@users.noreply.github.com>
2023-03-13 12:36:25 -0300

    Aprendendo estruturar de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+//realizando testes de repetição
+
+/*
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+*/
+
+/* com teste logico no inicio.
+
+var c = 1
+while(c <= 7){
+    console.log(`tudo bem! ${c}`)
+    c++
+}
+
+*/
+
+/* com teste logico no final
+
+var c = 1
+do{
+    console.log(`tudo bem! ${c}`)
+    c++
+}while(c <= 7)
+
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,8 @@
+// utilizando o FOR
+console.log('Vai começar...')
+
+for (var c=1; c <= 5; c++){
+    console.log(`tudo bem! ${c}`)
+}
+
+console.log('FIM!')
